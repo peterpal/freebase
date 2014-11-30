7357e210e7fbf4ac771efaeb224c2cb9fd7e5772
Veronika Strbakova <veronikastrbakova@gmail.com>
2014-11-30 11:03:40 +0100

    Added missing class result.rb

---
+++ ruby/src/result.rb
@@ -0,0 +1,18 @@
+class Result
+
+  def person= person
+    @person = person
+  end
+
+  def score= score
+    @score = score
+  end
+
+  def get_person
+    return @person
+  end
+
+  def get_score
+    return @score
+  end
+end
