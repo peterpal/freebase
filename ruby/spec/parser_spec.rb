99978a684f6a2f71cdbfe158152177eacdf009ed
Veronika Strbakova <veronikastrbakova@gmail.com>
2014-11-30 16:35:53 +0100

    Repaired unit tests

--- ruby/spec/parser_spec.rb
+++ ruby/spec/parser_spec.rb
@@ -1,16 +1,18 @@
-require 'test/spec'
+# To run this test you need to install rspec gem (gem install rspec).
+# Then run command rspec path_to_rspec_file/name
+# In console opened in spec directory run rspec parser_spec.rb
+
 require '../../ruby/src/parser'
 
-context 'Parser' do
-
-  specify 'should parse person data to small txt file' do
-    fixture = '../../data/people.txt'
-    p = Parse.new
+describe :parser do
+  it 'should parse person data to small txt file' do
+    input = '../../data/people'
+    p = Parser.new
     p.path_birth = '../../data/sample_date_of_birth.txt'
     p.path_death = '../../data/sample_date_of_death.txt'
-    p.path_names = '../../data/sample_names.txt.'
+    p.path_names = '../../data/sample_names.txt'
     p.Parse
     output = '../../data/data.txt'
-    expect(output).to be_eql File.read(fixture)
+    expect(File.read(output)).to be_eql File.read(input)
   end
 end
