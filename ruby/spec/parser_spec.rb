4246544da5b3b0e5fe113ef669fabf09e097538b
Veronika Strbakova <veronikastrbakova@gmail.com>
2014-12-07 10:11:19 +0100

    Comment changed in test

--- ruby/spec/parser_spec.rb
+++ ruby/spec/parser_spec.rb
@@ -1,6 +1,13 @@
 # To run this test you need to install rspec gem (gem install rspec).
+# Then you need to prepare 4 files from freebase dump:
+#   1. file with triplets, which contains date of birth (ns/people.person.date_of_birth)
+#   2. file with triplets, which contains date of death (ns/people.deceased_person.date_of_death)
+#   3. file with english names (type.object.name)
+#   4. file with people data (id;name;date_of_birth;date_of_death), which has complete data (skip person, who has null data)
 # Then run command rspec path_to_rspec_file/name
 # In console opened in spec directory run rspec parser_spec.rb
+# From files with date of birth, date of death and names are parsed people data and connected to form (id;name;date_of_birth;date_of_death)
+# This output is compared to your prepared file with people data, and you get true for same results, false, when an error has occured
 
 require '../../ruby/src/parser'
 
